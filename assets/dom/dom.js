6057f67d3079dc60be49a70860f1c09fa46b76a1
Emilio Casillas <emilio.casi20@icloud.com>
2023-02-02 19:05:06 -0600

    Practica de dom con boostrap y poquito queryselect

---
+++ assets/dom/dom.js
@@ -0,0 +1,25 @@
+let serie_1 = document.getElementById('serie_1')
+serie_1.innerHTML('Esto es una caja serie desde JS')
+console.log(serie_1)
+
+
+
+
+
+
+let serie_2 ;
+serie_2 = document.getElementsByClassName('card')
+
+for(let i=0; i<serie_2.length; i++){
+    console.log(serie_2[i])
+}
+
+
+
+/*
+let serie_3 = document.getElementById('serie_3')
+serie_3.innerHTML('Esto es una caja serie desde JS')
+console.log(serie_3)
+*/
+
+
